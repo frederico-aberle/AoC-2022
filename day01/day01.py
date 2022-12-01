0911bbb72276adc06daaa80b98d3fb75c3ffbc50
Frederico Aberle <faberle@student.ethz.ch>
2022-12-01 10:53:56 +0100

    …

--- day01/day01.py
+++ day01/day01.py
@@ -1,5 +1,25 @@
-file = open("input01.txt")
-t = int(file.readline())
-for i in range(t):
-    a, b = map(int, file.readline().split())
-    print(a, b)
+file = [line.strip() for line in open("input01.txt").readlines()]
+
+"""Part One"""
+res = float('-inf')
+cal = 0
+for line in file:
+    if line == '':
+        res = max(res, cal)
+        cal = 0
+        continue
+    cal += int(line)
+res = max(res, cal)
+print(res)
+
+"""Part Two"""
+calories = []
+cal = 0
+for line in file:
+    if line == '':
+        calories.append(cal)
+        cal = 0
+        continue
+    cal += int(line)
+calories.append(cal)
+print(sum(sorted(calories)[-3:]))
