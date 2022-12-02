14d5a9519cf24f6b6b4a2f4a1ecda9122e07b889
Frederico Aberle <faberle@student.ethz.ch>
2022-12-02 14:16:24 +0100

    …

---
+++ day02/day02.py
@@ -0,0 +1,22 @@
+file = [line.strip().split() for line in open("input02.txt").readlines()]
+
+"""Part One"""
+res = 0
+for line in file:
+    op = line[0]
+    me = line[1]
+    res += ord(me) - ord('X') + 1
+    mapped = chr(ord(op) - ord('A') + ord('X'))
+    res += ((ord(me) - ord(mapped) + 1) % 3) * 3
+print(res)
+
+"""Part Two"""
+res = 0
+for line in file:
+    op = line[0]
+    outcome = line[1]
+    mapped = chr(ord(op) - ord('A') + ord('X'))
+    me = chr(ord('A') + (ord(outcome) - ord('X') + ((ord(op) - ord('B')) % 3)) % 3)
+    res += ord(me) - ord('A') + 1
+    res += (ord(outcome) - ord('X')) * 3
+print(res)
