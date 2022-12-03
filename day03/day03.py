daa0b93f05380aa3d8e9264b71689a740d305948
Frederico Aberle <faberle@student.ethz.ch>
2022-12-03 11:53:41 +0100

    …

--- day03/day03.py
+++ day03/day03.py
@@ -1,22 +1,20 @@
-file = [line.strip().split() for line in open("input03.txt").readlines()]
+file = [line.strip() for line in open("input03.txt").readlines()]
 
 """Part One"""
 res = 0
 for line in file:
-    op = line[0]
-    me = line[1]
-    res += ord(me) - ord('X') + 1
-    mapped = chr(ord(op) - ord('A') + ord('X'))
-    res += ((ord(me) - ord(mapped) + 1) % 3) * 3
+    comp_one = line[:len(line)//2]
+    comp_two = line[len(line)//2:]
+    common = list(set(comp_one).intersection(comp_two))[0]
+    res += ord(common) - ord('a') + 1 if common.islower() else ord(common) - ord('A') + 27
 print(res)
 
 """Part Two"""
 res = 0
-for line in file:
-    op = line[0]
-    outcome = line[1]
-    mapped = chr(ord(op) - ord('A') + ord('X'))
-    me = chr(ord('A') + (ord(outcome) - ord('X') + ((ord(op) - ord('B')) % 3)) % 3)
-    res += ord(me) - ord('A') + 1
-    res += (ord(outcome) - ord('X')) * 3
+for i in range(len(file)//3):
+    bag_one = file[3*i]
+    bag_two = file[3*i+1]
+    bag_thr = file[3*i+2]
+    common = list(set(bag_one).intersection(bag_two).intersection(bag_thr))[0]
+    res += ord(common) - ord('a') + 1 if common.islower() else ord(common) - ord('A') + 27
 print(res)
